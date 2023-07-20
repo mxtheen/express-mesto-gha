22c1308fd542bc599095c197773c4861313bbca5
Maxim Shuliaev <maximshuliaev@gmail.com>
2023-07-20 21:03:24 +0300

    Reworked after review 2

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,23 +1,20 @@
 /* eslint-disable consistent-return */
 const jwt = require('jsonwebtoken');
-const { UNAUTHORIZED } = require('../utils/statusCodes');
+const UnauthorizedError = require('../utils/errors/UnauthorizedError');
 
-const handleAuthError = (res) => {
-  res.status(UNAUTHORIZED).send({ message: 'Ошибка авторизации' });
-};
 const extractBearerToken = (header) => header.replace('Bearer ', '');
 
 const authMiddleware = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return handleAuthError(res);
+    return next(new UnauthorizedError('Ошибка авторизации.'));
   }
   const token = extractBearerToken(authorization);
   let payload;
   try {
     payload = jwt.verify(token, 'super-strong-secret');
   } catch (err) {
-    return handleAuthError(res);
+    return next(new UnauthorizedError('Ошибка авторизации.'));
   }
 
   req.user = payload;
